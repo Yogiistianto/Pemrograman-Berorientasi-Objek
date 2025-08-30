4591b18d5ca71e2d03b05d8b6d4d8629dd1098d9
Yogi Istianto <yogiskarton@gmail.com>
2025-08-31 01:52:40 +0700

    Add files via upload

---
+++ Biodata_Yogi_project1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biodata_yogi_project1;
+
+/**
+ *
+ * @author HP
+ */
+public class Biodata_Yogi_project1 {
+    public static void main(String[] args) {
+        System.out.println("Nama   : Yogi Istianto");
+        System.out.println("NPM    : 123456789");
+        System.out.println("Alamat : Jl. Contoh No. 21, Jakarta");
+        System.out.println("No_HP  : 081234567890");
+    }
+}
+
