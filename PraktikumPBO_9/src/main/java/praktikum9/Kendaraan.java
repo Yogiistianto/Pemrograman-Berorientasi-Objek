e2b93c3eb7ec910c955ac52cc9aea048460ab88b
Yogi Istianto <yogiskarton@gmail.com>
2025-11-10 23:11:43 +0700

    Add files via upload

---
+++ PraktikumPBO_9/src/main/java/praktikum9/Kendaraan.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author HP
+ */
+//Definisi kelas abstrak
+abstract class Kendaraan {
+    //Metode kelas abstrak
+    abstract void berjalan();
+    
+    //Metode dengan implementasi
+    void info() {
+        System.out.println("Ini adalah Kendaraan.");
+    }
+    
+}
---
+++ PraktikumPBO_9/src/main/java/praktikum9/Mobil.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktikum9;
+
+/**
+ *
+ * @author HP
+ */
+//Kelas turunan dari Kendaraan
+class Mobil extends Kendaraan {
+    @Override
+    void berjalan(){
+        System.out.println("Mobil sedang berjalan dengan cepat");
+    }
+}
